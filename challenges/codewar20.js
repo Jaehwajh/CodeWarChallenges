deefd003234b53123acaa8c2bd6692a09d2ab2b0
James Hwang <jaehwa.jh@gmail.com>
2022-05-04 14:47:08 -0700

    codewar challenge 5/4/2022 x3

---
+++ challenges/codewar20.js
@@ -0,0 +1,50 @@
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    // your code here
+    return Math.abs(dadYearsOld - sonYearsOld * 2)
+  }
+
+
+
+/*
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+
+
+*/
+
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    return arrayOfSheep.filter((x) => x === true).length
+  }
+
+
+/*
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+*/
+
+function cockroachSpeed(s) {
+    //Good Luck!
+    return Math.floor(s * 27.77778)
+  }
