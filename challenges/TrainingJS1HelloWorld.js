daaed6b314157e4f82a22b5fe0cc6942b57b3e2a
James Hwang <jaehwa.jh@gmail.com>
2022-05-16 14:13:35 -0700

    kyu 8 codewar challenge 5/16/2022

---
+++ challenges/TrainingJS1HelloWorld.js
@@ -0,0 +1,26 @@
+/*
+Task
+Please refer to two example above and write your first JS function.
+
+mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line(don't forget to put the str in the brackets).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+In the end, click "Submit" to submit your code pass this kata.
+*/
+
+function helloWorld(){
+    var str = 'Hello World!'
+    
+    console.log(str)
+  }
