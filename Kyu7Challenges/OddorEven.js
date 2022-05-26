cfb9b572295f06c099b8c9607b69dab160a62fc3
James Hwang <jaehwa.jh@gmail.com>
2022-05-26 12:38:56 -0700

    Kyu 7 codewar challenge 5/26/2022 Odd or even

---
+++ Kyu7Challenges/OddorEven.js
@@ -0,0 +1,30 @@
+/*
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+Have fun!
+*/
+
+function oddOrEven(array) {
+    //enter code here
+   let total = array.reduce((a,ac) => a + ac, 0)
+   
+   if(total % 2 === 0){
+     return 'even'
+   }else {
+     return 'odd'
+   }
+ }
