470b5dc420f820235804036f7ec89cde3fa43a9e
James Hwang <jaehwa.jh@gmail.com>
2022-06-01 16:28:04 -0700

    Kyu 8 codewar challenge 6/1/2022 Exclamation serkies #2

---
+++ challenges/RemoveAllExclamationMarksfromtheEndofSentence.js
@@ -0,0 +1,16 @@
+/*
+Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+function remove (string) {  
+    return string.replace(/!*$/g,'')
+   }
