cab88d01030d7b0ab195a393d0554e945a965717
James Hwang <jaehwa.jh@gmail.com>
2022-06-02 17:47:40 -0700

    Kyu 8 codewar challenge 6/2/2022 Find the position!

---
+++ challenges/FindThePosition.js
@@ -0,0 +1,15 @@
+/*
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+This kata is meant for beginners. Rank and upvote to bring it out of beta
+*/
+
+function position(letter){
+    //Write your own Code!
+      
+      return `Position of alphabet: ${letter.charCodeAt()-96}`
+    }
