4fb79a0b3c167fb90ce5ca774a618f9773145a47
James Hwang <jaehwa.jh@gmail.com>
2022-06-05 13:50:30 -0700

    Kyu 8 codewar challenge 6/5/2022 Sum of multiples

---
+++ Kyu8Challenges/SumOfMultiples.js
@@ -0,0 +1,28 @@
+/*
+Your Job
+Find the sum of all multiples of n below m
+
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+Examples
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+
+*/
+
+function sumMul(n,m){
+    let total = 0
+  //your idea here
+   if(n <= 0 || m <= 0){
+     return 'INVALID'
+   }else{
+     for(let i = n; i < m; i+=n){
+        total += i
+     }
+   }
+    
+    return total
+  }
