43cf2cbd08bd324e5d830ecceb970b91d609d4b4
James Hwang <jaehwa.jh@gmail.com>
2022-06-06 00:00:30 -0700

    Kyu 8 codewar challenge 6/6/2022 Sum of difference in array

---
+++ Kyu8Challenges/SumOfDifferenceinArray.js
@@ -0,0 +1,19 @@
+/*
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+Example
+[2, 1, 10]  -->  9
+In descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+*/
+
+function sumOfDifferences(arr) {
+    if(arr.length > 1){
+      return Math.max(...arr) - Math.min(...arr)
+    }else {
+      return 0
+    }
+  }
