743be1c3cddba1f413d2ee82c57554f8e18a19cb
James Hwang <jaehwa.jh@gmail.com>
2022-06-09 18:07:35 -0700

    Kyu 8 codewar challenge Sum of angles

---
+++ Kyu7Challenges/SumOfAngles.js
@@ -0,0 +1,8 @@
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+
+function angle(n) {
+    //code here
+    return (n-2) * 180
+  }
