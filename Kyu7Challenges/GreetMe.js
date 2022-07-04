39786d1ccc3f876e51cb5cefe6f8e5d474bf401a
James Hwang <jaehwa.jh@gmail.com>
2022-07-03 21:55:25 -0700

    Kyu 7 codewar challenge 6/3/2022 Greet Me

---
+++ Kyu7Challenges/GreetMe.js
@@ -0,0 +1,14 @@
+/*
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+*/
+
+var greet = function(name) {
+    let properName = name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()
+    
+    return `Hello ${properName}!`
+  };
