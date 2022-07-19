ccffdf9ded8ae8376d2de27c8cfdd69e3b4ea9b1
James Hwang <jaehwa.jh@gmail.com>
2022-07-18 21:42:54 -0700

    Kyu 7 codewar challenge 7/18/2022 Largest pair sum in array

---
+++ Kyu7Challenges/LargestPairSuminArray.js
@@ -0,0 +1,17 @@
+/*
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+Input sequence contains minimum two elements and every element is an integer.
+*/
+
+function largestPairSum (numbers) {
+    //TODO: Write your Code here
+    
+    let sortedNum = numbers.sort((a,b) => b - a)
+    
+    return sortedNum[0] + sortedNum[1]
+  }
