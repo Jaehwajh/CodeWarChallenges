f0d1e018e4fb1de4bafd134e42040821b71107ea
James Hwang <jaehwa.jh@gmail.com>
2022-08-01 12:54:01 -0700

    Kyu 8 codewar challenge 8/1/2022 Contamination String

---
+++ Kyu8Challenges/ContaminationString.js
@@ -0,0 +1,20 @@
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+function contamination(text, char){
+    // Code here ;)
+    
+    return text.split('').map(x => char).join('')
+    
+  }
