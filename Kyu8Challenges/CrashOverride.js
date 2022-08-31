ee4db24297f4dfe3e262b4c4da0dd870c6162b16
James Hwang <jaehwa.jh@gmail.com>
2022-08-30 23:38:02 -0700

    Kyu 8 codewar challenge 8/30/2022 Crash Override

---
+++ Kyu8Challenges/CrashOverride.js
@@ -0,0 +1,20 @@
+/*
+Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+
+If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+*/
+
+const firstLetter = str => str[0].toUpperCase()
+
+const isValidName = name => /[a-z]/gi.test(name)
+
+const aliasGen = (fName, sName) =>
+  isValidName(fName[0]) && isValidName(sName[0])
+    ? `${firstName[firstLetter(fName)]} ${surname[firstLetter(sName)]}`
+    : 'Your name must start with a letter from A - Z.'
