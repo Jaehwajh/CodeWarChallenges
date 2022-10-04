dae941ac1cb5acc7b186585dccf4311dd2446d48
James Hwang <jaehwa.jh@gmail.com>
2022-10-04 14:55:24 -0700

    kyu 7 tea for two

---
+++ Kyu7Challenges/TeaforTwo.js
@@ -0,0 +1,25 @@
+/*
+Tea for two!
+
+Given a string or number write a function tea42 that takes as input a string or number and returns a string where every '2' charcter or digit has been replaced with a t.
+
+tea42('coffee') //should return 'coffee'
+tea42('tea') //should return 'tea'
+tea42('2ea') //should return 'tea'
+tea42(9022) //should return '90tt'
+tea42(5676765) //should return '5676765'
+tea42('2u2u') //should return 'tutu'
+*/
+
+function tea42(input) {
+    // your code
+    let two = input.toString().split('')
+    
+    for(let i = 0; i < two.length; i++){
+      if(two[i] === '2'){
+        two[i] = 't'
+      }
+    }
+    
+    return two.join('')
+  };
