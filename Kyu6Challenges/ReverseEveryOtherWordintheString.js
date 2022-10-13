78293652c080cff158d0ce37fec8a88bbcef94b2
James Hwang <jaehwa.jh@gmail.com>
2022-10-13 16:19:21 -0700

    Kyu 6 Reverse every other word in the string

---
+++ Kyu6Challenges/ReverseEveryOtherWordintheString.js
@@ -0,0 +1,9 @@
+/*
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+
+function reverse(str){
+    //WRITE SOME MAGIC
+    return str.split(' ').map((element,index) => index % 2 === 1 ? element.split('').reverse().join('') : element).join(' ').trim()
+      
+  }
