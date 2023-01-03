821be7e114ef28b7557a81c078d37ca6fae3673d
Jaehwajh <jaehwa.jh@gmail.com>
2023-01-03 07:46:41 -0800

    Kyu 8 Get the mean of an array

---
+++ Kyu8Challenges/GettheMeanofanArray.js
@@ -0,0 +1,10 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+function getAverage(marks){
+    //TODO : calculate the downward rounded average of the marks array
+    return parseInt(marks.reduce((a,b) => a + b)/marks.length)
+  }
