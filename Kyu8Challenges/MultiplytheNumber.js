c62f60bc0409c2e1ac191365b0ca5faff0312e30
Jaehwajh <jaehwa.jh@gmail.com>
2023-01-19 16:47:02 -0800

    Kyu 8 Multiply the number

---
+++ Kyu8Challenges/MultiplytheNumber.js
@@ -0,0 +1,15 @@
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+// multiply(3)==15 // 3 * 5¹
+// multiply(10)==250 // 10 * 5²
+// multiply(200)==25000 // 200 * 5³
+// multiply(0)==0 // 0 * 5¹
+// multiply(-3)==-15 // -3 * 5¹
+
+function multiply(number){
+    //your code here
+    let power = Math.abs(number).toString().length
+    
+    return number * (5**power)
+  
+  }
