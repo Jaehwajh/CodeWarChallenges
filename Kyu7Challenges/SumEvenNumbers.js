e5f400c9d5af73354346c4fd15088123571fda59
Jaehwajh <jaehwa.jh@gmail.com>
2023-01-28 23:24:10 -0800

    Kyu 7 Sum Even Numbers

---
+++ Kyu7Challenges/SumEvenNumbers.js
@@ -0,0 +1,10 @@
+// Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+
+// Only numbers without decimals like 4 or 4.0 can be even.
+
+// The input is a sequence of numbers: integers and/or floats.
+
+function sumEvenNumbers(input) {
+    // [...]
+    return input.filter(element => element % 2 === 0).reduce((a,b) => a+b,0)
+  }
